2933aa16271a438b46ad42005258462c4b28a2b5
MKAbuMattar <mohammad.khaled@outlook.com>
2021-05-19 17:24:11 +0300

    update books

--- server.js
+++ server.js
@@ -5,20 +5,27 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+
 //modules
-const handelUser = require('./modules/User/User');
+const userModel = require('./models/user.model/user.model');
 
 //env
 const PORT = process.env.PORT;
 
 const app = express();
+app.use(express.json());
 app.use(cors());
 
 app.get('/', function (req, res) {
   res.send('Hello World');
 });
 
-app.get('/books', handelUser);
+app.get('/books', userModel.handelUser);
 
+app.post('/books', userModel.addbook);
+
+app.delete('/books/:index', userModel.deleteBook);
+
+app.put('/books/:index', userModel.updateBook);
 
 app.listen(PORT);
